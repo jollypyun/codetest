2a148a64ee0b53ab91aea8db3036d3b5d552b83e
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2023-11-16 15:20:00 +0900

    Create 달리기 경주.py

---
+++ 달리기 경주.py
@@ -0,0 +1,20 @@
+# 링크 : 
+
+# 실패 사례 1 (9~13 시간 초과 실패) : 단순 스왑
+def solution(players, callings):
+    for i in callings:
+        idx = players.index(i)
+        temp = players[idx]
+        players[idx] = players[idx-1]
+        players[idx-1] = temp
+    return players
+
+# 실패 사례 2 (7~13 시간 초과 실패): dict, list 활용
+def solution(players, callings):
+    d = {j:i for i,j in enumerate(players)}
+    for i in callings:
+        past = [k for k,v in d.items() if v == d[i] - 1].pop()
+        d[past] += 1
+        d[i] -=1
+    
+    return list(map(lambda y: y[0], sorted(d.items(), key = lambda x: x[1])))
