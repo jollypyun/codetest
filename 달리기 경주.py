c994ff91a535f5a3805988684a3a55bd09fa00dd
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2023-11-16 15:49:28 +0900

    Update 달리기 경주.py

--- 달리기 경주.py
+++ 달리기 경주.py
@@ -18,3 +18,17 @@ def solution(players, callings):
         d[i] -=1
     
     return list(map(lambda y: y[0], sorted(d.items(), key = lambda x: x[1])))
+
+# 성공 사례 - 순위에 대한 dict, player에 대한 dict 두 개를 활용해서 pop과 같은 시간이 오래 걸리는 탐색이나 변환 등 작업 시간을 줄인다. 또한, list가 아닌 dict를 사용해 탐색의 시간 복잡도를 줄인다.
+def solution(players, callings):
+    value_dict = {value: index for index, value in enumerate(players)}
+    index_dict = {index: value for index, value in enumerate(players)}
+    
+    for player in callings:
+        idx = value_dict[player]
+        past_player = index_dict[idx-1]
+        value_dict[player] -= 1
+        value_dict[past_player] += 1
+        index_dict[idx] = past_player
+        index_dict[idx-1] = player
+    return list(index_dict.values())
